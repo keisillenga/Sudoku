6129f437b2009ab5cac254ecefc0c39d3e737076
Kejsi Llenga <keisillenga@gmail.com>
2026-09-23 08:54:21 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const boardElement = document.getElementById("sudpku-board");
+const message = document.getElementById("message");
+const solveButton = document.getElementById("solveBtn");
+const checkButton = document.getElementById("checkBtn");
+const clearButton = document.getElementById("clearBtn");
+const exampleButton = document.getElementById("exampleBtn");
