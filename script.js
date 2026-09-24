5837c10acde38ddd80211ca9f1c5841ed4b73764
Kejsi Llenga <keisillenga@gmail.com>
2026-09-24 07:40:01 +0200

    script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const solveButton = document.getElementById("solveBtn");
 const checkButton = document.getElementById("checkBtn");
 const clearButton = document.getElementById("clearBtn");
 const exampleButton = document.getElementById("exampleBtn");
+
+function createBoard() {
+  boardElement.innerHTML = "";
+  for (let row = 0; row < 9; row++){
+    for (let col= 0; col < 9; col++){
+      const input = document.createElement("input");
+      
+      
+         
