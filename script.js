c5bdef95bf82f3bd139d2d9e8df4c073c1e004ea
Kejsi Llenga <keisillenga@gmail.com>
2026-09-24 10:20:56 +0200

    script.js

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ function createBoard() {
   for (let row = 0; row < 9; row++){
     for (let col= 0; col < 9; col++){
       const input = document.createElement("input");
+      input.type = "text";
+      input.classList.add("cell");
+      input.maxLength = 1;
+      input.dataset.row = row;
+      input.dataset.col = col;
+      input.addElementById("input", function () {
+        this.value = this.value.replace(/[^1-9]g, "");
+       });
+      boardElement.appendChild(input);
+    }
+  }
+}
+      
       
       
          
